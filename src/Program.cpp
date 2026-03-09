3f9ffaf847f1768c26c1d78e35a086d9f672a352
Luis <luis.aneses1@upr.edu>
2026-03-09 19:23:25 -0400

    Se "implementó" la lógica de los enemigos cuando el juego hace reset.

--- src/Program.cpp
+++ src/Program.cpp
@@ -186,6 +186,32 @@ void Program::PlayerReset() {
 
 void Program::Reset() {
     Enemy::enemies.clear();
+
+    // Se copió el código de arriba para que los enemigos vuelvan a aparecer
+    // con su lógica implementada.
+
+        Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+                std::pair<float, float>{350, 150}, 
+                new SpEnemy(350, 150)
+            });
+        
+        Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+                std::pair<float, float>{600, 150}, 
+                new SpEnemy(600, 150)
+            });
+        
+        for (int i = 0; i < 30; i++) {
+            float x = 250 + 50 * (i%10); 
+            float y = 200 + 50 * (i/10); 
+        
+            Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+                std::pair<float, float>{x, y}, 
+                new StdEnemy(x, y)
+            });
+        }
+
+    // Termina bloque de código copiado.
+
     StdEnemy::attackInProgress = false;
     player = new Player((GetScreenWidth() / 2) - 15, GetScreenHeight() * 0.75f);
     respawnCooldown = 1080;
