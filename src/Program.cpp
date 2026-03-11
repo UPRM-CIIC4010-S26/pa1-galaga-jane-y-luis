b479573f7709df854ed651a372e11326dd888df5
Luis <luis.aneses1@upr.edu>
2026-03-10 20:36:38 -0400

    Se implementó el score y la 'K' para que sume 500 puntos.

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,5 +1,7 @@
 #include "Program.hpp"
 
+int score = 0;
+
 Program::Program() {
     Background::sideWalls = std::pair<HitBox, HitBox>{ 
         HitBox(0, 0, 10, GetScreenHeight()), 
@@ -158,6 +160,7 @@ void Program::KeyInputs() {
     if (!paused && !startup && IsKeyPressed('O')) gameOver = !gameOver;
     if (!gameOver && !paused && IsKeyPressed('I')) startup = !startup;
     if (IsKeyPressed('H')) HitBox::drawHitbox = !HitBox::drawHitbox;
+    if (IsKeyPressed('K')) score += 500; // Suma 500 puntos si se aprieta la 'K'.
     
     if (gameOver && IsKeyPressed(KEY_ENTER)) {
         gameOver = false;
@@ -194,4 +197,5 @@ void Program::Reset() {
     count = 0;
     delay = 0;
     lives = 3;
+    score = 0; // resetea el score.
 }
