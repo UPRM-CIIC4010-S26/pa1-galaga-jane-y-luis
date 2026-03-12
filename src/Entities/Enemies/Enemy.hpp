33b6c9fd1624826950464b01988ce6e0c7767964
Luis <luis.aneses1@upr.edu>
2026-03-11 21:43:48 -0400

    Implementado el score cuando se puere un enemigo. Hay que ponerle un score a cada tipo de enemigo.

--- src/Entities/Enemies/Enemy.hpp
+++ src/Entities/Enemies/Enemy.hpp
@@ -5,6 +5,7 @@
 #include "Animation.hpp"
 #include <iostream>
 
+extern int score;
 class Enemy {
     protected:
         float angle = 90;
@@ -62,6 +63,7 @@ class Enemy {
                             Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
                         );
                         p.second = nullptr;
+                        score += 100; // Añade 100 de base. Se tiene que actualizar.
                     }
                 }
             }
